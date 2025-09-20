5e3b3b35f0a23b1bc2aa9be3690c082ca6cddfbe
Bill <billwang7599@gmail.com>
2025-09-21 02:27:42 +0800

    added select to check if buffer is full before sending

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <chrono>
-#include <thread>   // Required for std::this_thread
 
 // The main function takes two parameters to handle command-line arguments:
 // 1. argc (argument count): An integer for the number of arguments.
@@ -22,6 +21,15 @@
 std::string end_message = "FIN";
 
 int send_all(int sockfd, const char *data, size_t len) {
+    // check if socket is ready to accept more data
+    fd_set write_fds;
+    FD_ZERO(&write_fds);
+    FD_SET(sockfd, &write_fds);
+    if (!FD_ISSET(sockfd, &write_fds)) {
+       return -2; // full
+    }
+
+    // send data
     size_t total_sent = 0;
     while (total_sent < len) {
         int bytes_sent = send(sockfd, data + total_sent, len - total_sent, 0);
@@ -183,14 +191,16 @@ int client(char* hostname, char* port, int time_s) {
         }
 
         std::cout << "Sending data..." << std::endl;
-        if (send_all(sockfd, package, CHUNK_SIZE_BYTES) == -1) {
+        int flag = send_all(sockfd, package, CHUNK_SIZE_BYTES);
+        if (flag == -1) {
             std::cerr << "Failed to send data chunk." << std::endl;
             close(sockfd);
             freeaddrinfo(res);
             return -1;
+        } else if (flag == -2) {
+            std::cerr << "Socket buffer is full." << std::endl;
         }
         bytes_sent += CHUNK_SIZE_BYTES;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
     // send end message
